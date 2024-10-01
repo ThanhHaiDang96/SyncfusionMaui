6f96043fff73ba31caa6d846e3979aaeaa09aacc
HaiDT <hai.dt@eastgate-software.com>
2024-10-01 17:44:08 +0700

    Update sample

--- SyncfusionIssue/ViewModel/ImagePropertiesPopupViewModel.cs
+++ SyncfusionIssue/ViewModel/ImagePropertiesPopupViewModel.cs
@@ -1,9 +1,39 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Syncfusion.Maui.ImageEditor;
+using System.Windows.Input;
 
 namespace SyncfusionIssue.ViewModel;
 
 public partial class ImagePropertiesPopupViewModel : ObservableObject
 {
+    public TaskCompletionSource<byte[]?> ImageDataSource { get; set; } = new TaskCompletionSource<byte[]?>();
+
     [ObservableProperty] private byte[]? _sources;
-    
+
+    [RelayCommand]
+    private async Task Ok(object obj)
+    {
+        if (obj is SfImageEditor imageEditor)
+        {
+            imageEditor.SaveEdits();
+            
+            using (var memoryStream = new MemoryStream())
+            {
+                await using var stream = await imageEditor.GetImageStream();
+                await stream.CopyToAsync(memoryStream);
+                memoryStream.Position = 0;
+                ImageDataSource?.SetResult(memoryStream.ToArray());
+            }
+
+            await Shell.Current.Navigation.PopModalAsync(true);
+        }
+    }
+
+    [RelayCommand]
+    private async Task Cancel(object obj)
+    {
+        ImageDataSource?.SetResult(null);
+        await Shell.Current.Navigation.PopModalAsync();
+    }
 }
--- SyncfusionIssue/ViewModel/MainViewModel.cs
+++ SyncfusionIssue/ViewModel/MainViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Syncfusion.Drawing;
 using Syncfusion.Maui.PdfViewer;
+using Syncfusion.Pdf;
 using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Interactive;
 using Syncfusion.Pdf.Parsing;
+using Syncfusion.Pdf.Redaction;
 using PointF = Syncfusion.Drawing.PointF;
+using SizeF = Microsoft.Maui.Graphics.SizeF;
 
 namespace SyncfusionIssue.ViewModel
 {
@@ -56,6 +61,11 @@ namespace SyncfusionIssue.ViewModel
             }
 
             _pdfViewer.AnnotationMode = AnnotationMode.None;
+            await using var redactStream = RedactAnnotation(PdfStream);
+            if (redactStream != null)
+            {
+                _pdfViewer.LoadDocument(redactStream);
+            }
             var stream = await _fileService.SaveAsAsync(_pdfViewer, "SavedFile.pdf");
             if (stream == null)
             {
@@ -103,29 +113,19 @@ namespace SyncfusionIssue.ViewModel
             }
         }
 
-        private void SetupAddRedaction()
-        {
-            if (_pdfViewer != null)
-            {
-                _pdfViewer.AnnotationMode = AnnotationMode.Square;
-                _pdfViewer.AnnotationSettings.Square.Color = Microsoft.Maui.Graphics.Color.FromRgba("#4597F8FF");
-                _pdfViewer.AnnotationSettings.Square.FillColor = Colors.Transparent;
-                _pdfViewer.AnnotationSettings.Square.Opacity = 1.0f;
-                _pdfViewer.AnnotationSettings.Square.BorderWidth = 1.0f;
-            }
-        }
-
-        private void TurnOffAnnotation()
-        {
-            if (_pdfViewer != null)
-            {
-                _pdfViewer.AnnotationMode = AnnotationMode.None;
-            }
-        }
-
         [RelayCommand]
         private void PdfViewerAnnotationAdded(object obj)
         {
+            if (obj is (SfPdfViewer pdfViewer, AnnotationEventArgs { Annotation: not null } eventArgs))
+            {
+                _selectedAnnotation = eventArgs.Annotation;
+                if (eventArgs.Annotation is SquareAnnotation squareAnnotation)
+                {
+                    squareAnnotation.Name = $"Redaction{Guid.NewGuid()}";
+                    squareAnnotation.FillColor = Colors.White;
+                }
+                
+            }
         }
 
         [RelayCommand]
@@ -172,9 +172,111 @@ namespace SyncfusionIssue.ViewModel
             }
         }
 
-        private void RedactAnnotation()
+        [RelayCommand]
+        private async Task ShowCustomImage()
         {
-            
+            try
+            {
+                if (_selectedAnnotation is StampAnnotation stampAnnotation)
+                {
+                    await using var imageStream = GetImageStreamFromAnnotation(stampAnnotation);
+                    if (imageStream == null)
+                    {
+                        return;
+                    }
+                    using var memoryStream = new MemoryStream();
+                    imageStream.Position = 0;
+                    await imageStream.CopyToAsync(memoryStream);
+                    memoryStream.Position = 0;
+                    var imagePropertiesPopupViewModel = new ImagePropertiesPopupViewModel()
+                    {
+                        Sources = memoryStream.ToArray(),
+                        ImageDataSource = new TaskCompletionSource<byte[]?>(memoryStream.ToArray()),
+                    };
+
+                    await Shell.Current.Navigation.PushModalAsync(new ImagePropertiesPopup()
+                    {
+                        BindingContext = imagePropertiesPopupViewModel,
+                    });
+
+                    var data = await imagePropertiesPopupViewModel.ImageDataSource.Task;
+                    if (data != null)
+                    {
+                        UpdateImageAnnotation(data, stampAnnotation);
+                    }
+
+                }
+               
+            }
+            catch (Exception )
+            {
+                //
+            }
+        }
+
+        private void UpdateImageAnnotation(byte[] data, StampAnnotation stampAnnotation)
+        {
+            var stream = new MemoryStream(data);
+            var newAnnotation = new StampAnnotation(stream, stampAnnotation.PageNumber, stampAnnotation.Bounds)
+            {
+                Name = stampAnnotation.Name,
+            };
+            _pdfViewer?.AddAnnotation(newAnnotation);
+            _pdfViewer?.RemoveAnnotation(stampAnnotation);
+
+            _selectedAnnotation = newAnnotation;
+        }
+
+        private Stream? RedactAnnotation(Stream? documentStream)
+        {
+            if (_pdfViewer == null)
+            {
+                return null;
+            }
+
+            var annotations = _pdfViewer.Annotations.Where(x => x.Name?.Contains("Redaction") == true).ToList();
+            if (annotations.Count != 0)
+            {
+                using var document = new PdfLoadedDocument(documentStream);
+
+                foreach (var annotation in annotations)
+                {
+                    // Type cast the annotation to get the annotation specific properties.
+                    if (annotation is SquareAnnotation squareAnnotation)
+                    {
+                        //Get the first page from the document
+                        if (document.Pages[annotation.PageNumber - 1] is PdfLoadedPage page)
+                        {
+                            var redaction = new PdfRedaction(
+                                bounds: new RectangleF(squareAnnotation.Bounds.X, 
+                                    squareAnnotation.Bounds.Y, 
+                                    squareAnnotation.Bounds.Width, 
+                                    squareAnnotation.Bounds.Height))
+                            {
+                                //Set fill color for the redaction bounds 
+                                FillColor =  Syncfusion.Drawing.Color.MediumPurple,
+                            };
+                            //Add a redaction object into the redaction collection of loaded page
+                            page.AddRedaction(redaction);
+                            //Redact the contents from the PDF document
+                            document.Redact();
+                        }
+                    }
+                    
+                }
+
+                //Creating the stream object
+                MemoryStream stream = new MemoryStream();
+                //Save the document
+                document.Save(stream);
+
+                //Close the document
+                document.Close(true);
+
+                return stream;
+            }
+
+            return null;
         }
 
         private void ClearTemporary()
@@ -246,6 +348,59 @@ namespace SyncfusionIssue.ViewModel
             }
 
             return new SizeF(width, height);
+        }
+        
+        private void SetupAddRedaction()
+        {
+            if (_pdfViewer != null)
+            {
+                _pdfViewer.AnnotationMode = AnnotationMode.Square;
+                _pdfViewer.AnnotationSettings.Square.Color = Microsoft.Maui.Graphics.Color.FromRgba("#4597F8FF");
+                _pdfViewer.AnnotationSettings.Square.FillColor = Colors.Transparent;
+                _pdfViewer.AnnotationSettings.Square.Opacity = 1.0f;
+                _pdfViewer.AnnotationSettings.Square.BorderWidth = 1.0f;
+            }
+        }
+
+        private void TurnOffAnnotation()
+        {
+            if (_pdfViewer != null)
+            {
+                _pdfViewer.AnnotationMode = AnnotationMode.None;
+            }
+        }
+
+        private Stream? GetImageStreamFromAnnotation(StampAnnotation? stampAnnotation)
+        {
+            if (stampAnnotation == null || _pdfViewer == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                using Stream mainStream = new MemoryStream();
+                _pdfViewer.SaveDocument(outputStream: mainStream);
+                using var pdfLoadedDocument = new PdfLoadedDocument(mainStream);
+                var pageNumber = stampAnnotation.PageNumber;
+                if (pdfLoadedDocument.Pages[pageNumber - 1] is PdfLoadedPage page)
+                {
+                    foreach (var annotation in page.Annotations)
+                    {
+                        if (annotation is PdfLoadedRubberStampAnnotation pdfLoadedRubberStampAnnotation && (stampAnnotation.Name == pdfLoadedRubberStampAnnotation.Name))
+                        {
+                            return pdfLoadedRubberStampAnnotation.GetImages()?.FirstOrDefault();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //ignore
+            }
+
+
+            return null;
         }
     }
 }
